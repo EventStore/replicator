3ef7c549dc70d94932bdd774477b291ee8b7d72a
Alexey Zimarev <alex@zimarev.com>
2021-02-12 12:11:52 +0100

    Preparing the deployment

---
+++ src/es-replicator/ClientApp/src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            activeIndex: '1',
+            activeIndex2: '1'
+        };
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
+        }
+    }
+}
+</script>
---
+++ src/es-replicator/ClientApp/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+<!--    <el-row :gutter="30">-->
+<!--      <el-col :span="24">-->
+        <Dashboard />
+<!--      </el-col>-->
+<!--    </el-row>-->
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Dashboard from "@/components/Dashboard.vue";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    Dashboard,
+  },
+});
+</script>
