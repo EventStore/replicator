6dd5391ef9a6a9d53d5e0ec4ce9947c42cb82690
Alexey Zimarev <alex@zimarev.com>
2021-04-27 15:54:22 +0200

    Added diagnostics

--- src/es-replicator/Program.cs
+++ src/es-replicator/Program.cs
@@ -12,23 +12,27 @@ using es_replicator.Settings;
 using Microsoft.Extensions.Configuration;
 
 var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var isDebug     = Environment.GetEnvironmentVariable("REPLICATOR_DEBUG") != null;
+var logConfig   = new LoggerConfiguration();
+logConfig = isDebug ? logConfig.MinimumLevel.Debug() : logConfig.MinimumLevel.Information();
 
-var logConfig = new LoggerConfiguration()
-    .MinimumLevel.Information()
+logConfig = logConfig
     .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
     .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
     .MinimumLevel.Override("Grpc", LogEventLevel.Error)
     .Enrich.FromLogContext();
 
 logConfig = environment?.ToLower() == "development"
-    ? logConfig.WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} <s:{SourceContext}>;{NewLine}{Exception}")
+    ? logConfig.WriteTo.Console(
+        outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} <s:{SourceContext}>;{NewLine}{Exception}"
+    )
     : logConfig.WriteTo.Console(new RenderedCompactJsonFormatter());
 Log.Logger = logConfig.CreateLogger();
 
 try {
     var fileInfo = FileVersionInfo.GetVersionInfo(Assembly.GetExecutingAssembly().Location);
     Log.Information("Starting replicator {Version}", fileInfo.ProductVersion);
-    
+
     Host.CreateDefaultBuilder(args)
         .ConfigureWebHostDefaults(
             webBuilder => {
@@ -36,8 +40,10 @@ try {
                 webBuilder.UseStartup<Startup>();
             }
         )
-        .ConfigureAppConfiguration(config => config
-            .AddYamlFile("./config/appsettings.yaml", false, true).AndEnvConfig())
+        .ConfigureAppConfiguration(
+            config => config
+                .AddYamlFile("./config/appsettings.yaml", false, true).AndEnvConfig()
+        )
         .Build()
         .Run();
     return 0;
@@ -48,6 +54,7 @@ catch (Exception ex) {
     while (true) {
         await Task.Delay(5000);
     }
+
     return 1;
 }
 finally {
--- src/es-replicator/config/transform.js
+++ src/es-replicator/config/transform.js
@@ -1,13 +1,16 @@
 function transform(stream, eventType, data, meta) {
+    if (stream.length > 7) return undefined;
+    
     const event = JSON.parse(data);
     const newEvent = {
         ...event,
         Data1: `new${event.Data1}`,
         NewProp: `${event.Id} - ${event.Data2}`
     };
+    const et = stream.length <= 6 ? `V2.${eventType}` : null;
     return {
         stream: `transformed${stream}`,
-        eventType: `V2.${eventType}`,
+        eventType: et,
         data: JSON.stringify(newEvent),
         meta
     }
