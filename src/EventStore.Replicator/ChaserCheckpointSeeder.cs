6e11f41366e5db5064bc08d7c32e41ed599e127e
yreynhout <yves.reynhout@bittacklr.be>
2024-08-23 11:36:44 +0200

    clean up code

--- src/EventStore.Replicator/ChaserCheckpointSeeder.cs
+++ src/EventStore.Replicator/ChaserCheckpointSeeder.cs
@@ -15,33 +15,33 @@ public class ChaserCheckpointSeeder : ICheckpointSeeder {
     }
 
     public async ValueTask Seed(CancellationToken cancellationToken) {
-        if (!await _checkpointStore.HasStoredCheckpoint(cancellationToken)) {
-            if (File.Exists(_filePath)) {
-                using var fileStream = new FileStream(
-                    _filePath,
-                    FileMode.Open,
-                    FileAccess.Read,
-                    FileShare.ReadWrite
-                );
-
-                if (fileStream.Length == 8) {
-                    using var reader   = new BinaryReader(fileStream);
-                    var       position = reader.ReadInt64();
-                    await _checkpointStore.StoreCheckpoint(new Position(0L, (ulong)position), cancellationToken);
-                }
-                else {
-                    Log.Warn(
-                        "Seeding failed because the file at {FilePath} does not appear to be an 8-byte position file",
-                        _filePath
-                    );
-                }
-            }
-            else {
-                Log.Warn("Seeding failed because the file at {FilePath} does not exist", _filePath);
-            }
-        }
-        else {
+        if (await _checkpointStore.HasStoredCheckpoint(cancellationToken)) {
             Log.Info("Checkpoint already present in store, skipping seeding");
+            return;
         }
+
+        if (!File.Exists(_filePath)) {
+            Log.Warn("Seeding failed because the file at {FilePath} does not exist", _filePath);
+            return;
+        }
+        using var fileStream = new FileStream(
+            _filePath,
+            FileMode.Open,
+            FileAccess.Read,
+            FileShare.ReadWrite
+        );
+
+        if (fileStream.Length != 8) {
+            Log.Warn(
+                "Seeding failed because the file at {FilePath} does not appear to be an 8-byte position file",
+                _filePath
+            );
+
+            return;
+        }
+
+        using var reader   = new BinaryReader(fileStream);
+        var       position = reader.ReadInt64();
+        await _checkpointStore.StoreCheckpoint(new Position(0L, (ulong)position), cancellationToken);
     }
 }
